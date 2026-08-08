676088b55d5114c2d48666e53a34b7c66dcf6c5e
Rai Cristian <raycristian13@gmail.com>
2026-08-07 22:06:24 -0300

    Refresh PWA logo cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'dive-planner-runtime-v3';
-const APP_SHELL = ['./', './index.html', './style.css', './app.js', './municipios_pb.js', './manifest.webmanifest', './icon-192.svg', './icon-512.svg', './domar-logo.png'];
+const CACHE_NAME = 'dive-planner-runtime-v4';
+const APP_SHELL = ['./', './index.html', './style.css', './app.js', './municipios_pb.js', './manifest.webmanifest', './icon-192.svg', './icon-512.svg', './domar-logo-v2.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
@@ -17,5 +17,6 @@ self.addEventListener('fetch', event => {
     return response;
   }).catch(() => caches.match(event.request).then(response => response || caches.match('./index.html'))));
 });
+
 
 
